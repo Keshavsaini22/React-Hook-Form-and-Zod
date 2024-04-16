ef7399dcec3fbac133605fcf1f141a4cf7ae5fac
Keshavsaini22 <keshavsainikesu@gmail.com>
2024-04-16 18:57:02 +0530

    done till react hook form

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,8 +1,43 @@
+'use client'
+import { Box, Button, Stack, TextField } from '@mui/material'
+import { resolve } from 'path'
 import React from 'react'
+import { FieldValues, useForm } from 'react-hook-form'
 
 function Signup() {
+    const { register, handleSubmit, formState: { errors, isSubmitting }, reset, getValues } = useForm()
+    const onhandleSubmit = async (data: FieldValues) => {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        reset()
+    }
     return (
-        <div>Signup</div>
+        <Stack gap={2} p={4} component={'form'}
+            onSubmit={handleSubmit(onhandleSubmit)}
+        >
+            <Box sx={{ fontSize: '25px', fontWeight: '700' }}>Sign Up Form</Box>
+            <TextField label="Email" type='email' {...register('email', {
+                required: 'Email is required',
+                // minLength:{
+                //     value:5,
+                //     message:'Minimun length should be 5'
+                // }
+            })} />
+            {errors.email && (<p>{`${errors.email.message}`}</p>)}
+            <TextField label="Password" type='password' {...register('password', {
+                required: 'Password is required',
+                minLength: {
+                    value: 5,
+                    message: 'Password must be atleast 5 characters'
+                }
+            })} />
+            {errors.password && (<p>{`${errors.password.message}`}</p>)}
+            <TextField label="Confirm Password" type='password' {...register('confirm', {
+                required: 'Confirm password is required',
+                validate: (value) => value === getValues('password') || "Password must match"
+            })} />
+            {errors.confirm && (<p>{`${errors.confirm.message}`}</p>)}
+            <Button variant='contained' disabled={isSubmitting} type='submit'>Submit</Button>
+        </Stack>
     )
 }
 
